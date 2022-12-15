cb2bd4c64053b5741afd7dca736153b437ee5018
Neel Dandiwala <neeldandi@gmail.com>
2022-12-15 18:31:52 +0530

    deploy changes 3.0

--- dist/connection.js
+++ dist/connection.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.connection = void 0;
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
-exports.connection = new MongoClient('mongodb+srv://mongodb:mongodb@rrrcluster.nluljzi.mongodb.net/rrrdatabase?retryWrites=true&w=majority', { useNewUrlParser: true });
+exports.connection = new MongoClient('mongodb+srv://mongodb:mongodb@rrrcluster.nluljzi.mongodb.net/?', { useNewUrlParser: true });
 //# sourceMappingURL=connection.js.map
--- src/connection.ts
+++ src/connection.ts
@@ -1,3 +1,3 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
-export const connection = new MongoClient('mongodb+srv://mongodb:mongodb@rrrcluster.nluljzi.mongodb.net/rrrdatabase?retryWrites=true&w=majority', { useNewUrlParser: true });
+export const connection = new MongoClient('mongodb+srv://mongodb:mongodb@rrrcluster.nluljzi.mongodb.net/?', { useNewUrlParser: true });
